ea3fd463fa99d74e985621d776b5a7dc3334f37a
anmol-chhetri-G <anmolsinghchetri34@gmail.com>
2025-02-28 12:10:24 +0545

    Create network-check.sh

---
+++ Mini-Projects/Network Monitor/network-check.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+echo "Script started"
+
+log_file="log.csv"
+
+echo "Timestamp, Server, Status, Response Time" > "$log_file"
+
+# User input server names
+echo "Enter server addresses separated by spaces: "
+read -a servers
+
+for server in "${servers[@]}"; do
+    if ping_output=$(ping -c 1 "$server" 2>/dev/null); then
+        # Extract response time using awk
+        response_time=$(echo "$ping_output" | awk -F'time=' '/time=/ {print $2}' | awk '{print $1}')
+        
+        # If response_time is empty, set it to "N/A"
+        [[ -z "$response_time" ]] && response_time="N/A"
+
+        echo "$(date '+%Y-%m-%d %H:%M:%S'), $server, UP, ${response_time}ms" >> "$log_file"
+    else
+        echo "$(date '+%Y-%m-%d %H:%M:%S'), $server, Down" >> "$log_file"
+    fi
+done
+echo "Script Completed. Results saved to $log_file"
